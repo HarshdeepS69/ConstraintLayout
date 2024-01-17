82e809271aee24e7dbb5fdc4b6a77667488917c0
HarshdeepS69 <117998095+HarshdeepS69@users.noreply.github.com>
2024-01-17 18:54:19 +0530

    updated

--- app/src/main/java/com/harsh/constraintlayout/ConstraintWithChains.kt
+++ app/src/main/java/com/harsh/constraintlayout/ConstraintWithChains.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ConstraintWithChains : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_constraint_with_chains)
+        setContentView(R.layout.activity_constraint_task_17feb)
     }
 }
